4971ba7f4c23ce974ef3f96d6d3cdeb2dbcae7f1
RaffiRaffiRaffi <31465573+RaffiRaffiRaffi@users.noreply.github.com>
2018-12-30 15:57:33 +0600

    User List Recycler View first implementation.

---
+++ app/src/main/java/edu/universitydhaka/cse2216/lifeatcsedu/userListRecyclerViewAdapter.java
@@ -0,0 +1,80 @@
+package edu.universitydhaka.cse2216.lifeatcsedu;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class userListRecyclerViewAdapter extends RecyclerView.Adapter<userListRecyclerViewAdapter.ViewHolder>{
+
+    private static final String TAG = "Recycler View";
+
+    private ArrayList<String> mImages = new ArrayList<>();
+    private ArrayList<String> mImageNames = new ArrayList<>();
+    private Context context;
+
+    public userListRecyclerViewAdapter( Context context, ArrayList<String> mImages, ArrayList<String> mImageNames) {
+        this.mImages = mImages;
+        this.mImageNames = mImageNames;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.layout_listofitems,viewGroup,false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder,final int i) {
+
+        Log.d(TAG, "onBindViewHolder: " + i);
+        
+        Glide.with(context)
+                .asBitmap()
+                .load(mImages.get(i))
+                .into(viewHolder.userListImage);
+
+        viewHolder.userListName.setText(mImageNames.get(i));
+
+        viewHolder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //To Single User Page
+                Toast.makeText(context,mImageNames.get(i),Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mImageNames.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        CircleImageView userListImage;
+        TextView userListName;
+        RelativeLayout parentLayout;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            userListImage = itemView.findViewById(R.id.image);
+            userListName = itemView.findViewById(R.id.image_name);
+            parentLayout = itemView.findViewById(R.id.user_element_layout);
+        }
+    }
+}
--- app/src/main/java/edu/universitydhaka/cse2216/lifeatcsedu/userShowPage.java
+++ app/src/main/java/edu/universitydhaka/cse2216/lifeatcsedu/userShowPage.java
@@ -4,20 +4,27 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
 public class userShowPage extends Activity {
 
-    EditText userNameList;
-    String what;
-
     private DatabaseReference userPageDataBaseRef;
+    private FirebaseAuth userPageAuth;
+
+    private ArrayList<String> mImageURL = new ArrayList<>();
+    private ArrayList<String> mImageNames = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,18 +32,18 @@ public class userShowPage extends Activity {
         setContentView(R.layout.users_page);
 
         userPageDataBaseRef = FirebaseDatabase.getInstance().getReference("users");
-
-        userNameList = findViewById(R.id.listOfUserNames);
-        what = "";
+        userPageAuth = FirebaseAuth.getInstance();
 
         userPageDataBaseRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
                 for(DataSnapshot ds : dataSnapshot.getChildren()){
                     User user = ds.getValue(User.class);
-                    what = what + user.getName() + "\n";
+                    mImageNames.add(user.getName());
+                    mImageURL.add("https://i.kym-cdn.com/entries/icons/original/000/003/619/ForeverAlone.jpg");
                 }
-                userNameList.setText(what);
+                initRecyclerView();
             }
 
             @Override
@@ -45,4 +52,14 @@ public class userShowPage extends Activity {
             }
         });
     }
+
+    private void initRecyclerView(){
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerv_view);
+        userListRecyclerViewAdapter adapter = new userListRecyclerViewAdapter(this,mImageURL,mImageNames);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
 }
+
+// https://i.kym-cdn.com/entries/icons/original/000/003/619/ForeverAlone.jpg
